7aa2e4a77fe7c7ab309e47342ad10afae137680f
Gaurav Singh <gtx25801@gmail.com>
2023-02-03 13:39:26 +0530

    Compelted request methods

--- main.js
+++ main.js
@@ -1,51 +1,148 @@
+//AXIOS GLOBALS
+axios.defaults.headers.common['X-Auth-Token']="Some JWT Token";
+
 // GET REQUEST
 function getTodos() {
-  console.log('GET Request');
+  // axios({
+  //   method: "get",
+  //   url: "https://jsonplaceholder.typicode.com/todos",
+  //   params: {
+  //     _limit:5
+  //   }
+  // })
+  axios.get("https://jsonplaceholder.typicode.com/todos",{params:{_limit:5}},{timeout:5})
+  .then(res=>showOutput(res))
+  .catch(err=>console.error(err));
 }
 
 // POST REQUEST
 function addTodo() {
-  console.log('POST Request');
+  // axios({
+  //   method:"post",
+  //   url:"https://jsonplaceholder.typicode.com/todos",
+  //   data:{
+  //     title:"New Todo",
+  //     completed:false
+  //   }
+  // })
+  axios.post("https://jsonplaceholder.typicode.com/todos",
+  {
+    title:"New Todo",
+    completed:false
+  })
+  .then(res=>showOutput(res))
+  .catch(err=>console.error(err));
 }
 
 // PUT/PATCH REQUEST
 function updateTodo() {
-  console.log('PUT/PATCH Request');
+  axios.patch(
+    "https://jsonplaceholder.typicode.com/todos/1",{
+      title:"Updated Todo",
+      completed:false
+    }
+  )
+  .then(res=>showOutput(res))
+  .catch(err=>console.error(err));
 }
 
 // DELETE REQUEST
 function removeTodo() {
-  console.log('DELETE Request');
+  axios.delete(
+    "https://jsonplaceholder.typicode.com/todos/1"
+  )
+  .then(res=>showOutput(res))
+  .catch(err=>console.error(err));
 }
 
 // SIMULTANEOUS DATA
 function getData() {
-  console.log('Simultaneous Request');
+  axios.all([
+    axios.get("https://jsonplaceholder.typicode.com/todos",{params:{_limit:5}}),
+    axios.get("https://jsonplaceholder.typicode.com/posts",{params:{_limit:5}})
+  ])
+  .then(axios.spread((todos,posts)=>showOutput(todos)))
+  .catch(err=>console.error(err));
 }
 
 // CUSTOM HEADERS
 function customHeaders() {
-  console.log('Custom Headers');
+  const config={
+    headers:{
+      "Content-Type":"application/json",
+      Authorization:"Token"
+    }
+  }
+  axios.post("https://jsonplaceholder.typicode.com/todos",
+  {
+    title:"New Todo",
+    completed:false
+  },config)
+  .then(res=>showOutput(res))
+  .catch(err=>console.error(err));
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
 function transformResponse() {
-  console.log('Transform Response');
+  const options= {
+    method:"post",
+    url:"https://jsonplaceholder.typicode.com/todos",
+    data:{
+      title:"Transform",
+      completed:false
+    },
+    transformResponse: axios.defaults.transformResponse.concat(data=>{
+      data.title= data.title.toUpperCase();
+      return data;
+    })
+  };
+  axios(options)
+  .then(res=>showOutput(res));
 }
 
 // ERROR HANDLING
 function errorHandling() {
-  console.log('Error Handling');
+  axios.get("https://jsonplaceholder.typicode.com/todos/s",{params:{_limit:5}})
+  .then(res=>showOutput(res))
+  .catch(err=>{
+    if(err.response){
+      //server responded other than 200 range
+      console.log(err.response.data);
+      console.log(err.response.staus);
+      console.log(err.response.headers);
+    }
+  });
+  
 }
 
 // CANCEL TOKEN
 function cancelToken() {
-  console.log('Cancel Token');
+  const source = axios.CancelToken.source();
+  axios.get("https://jsonplaceholder.typicode.com/todos",{params:{_limit:5}},{cancelToken:source.Token})
+  .then(res=>showOutput(res))
+  .catch(thrown=>{
+    if(axios.isCancel(thrown)){
+      console.log("Cancelled",thrown.message);
+    }
+  });
+  if(true){
+    source.cancel("Request Cacelled");
+  }
 }
 
 // INTERCEPTING REQUESTS & RESPONSES
+axios.interceptors.request.use(config=>{
+  console.log(`${config.method.toUpperCase()} request sent to ${config.url} at ${new Date().getTime()}`);
+  return config;
+}, error=>{
+  return Promise.reject(error);
+})
 
 // AXIOS INSTANCES
+const axiosInstance = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com"
+});
+axiosInstance.get("/comments").then(res=>showOutput(res));
 
 // Show output in browser
 function showOutput(res) {
